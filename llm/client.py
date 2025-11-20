83939c3f1f482ae1bed43c2170a6a0e4c6e4037b
Darya Dmitrieva <144445820+dmitrievaaxx@users.noreply.github.com>
2025-11-20 17:31:13 +0300

    Update client.py

--- llm/client.py
+++ llm/client.py
@@ -14,6 +14,7 @@ load_dotenv()
 
 
 MODELS = [
+    "openai/gpt-5.1-chat"
     "meta-llama/llama-3.3-70b-instruct:free",
     "deepseek/deepseek-r1-0528-qwen3-8b:free",
     "openrouter/sherlock-dash-alpha",
